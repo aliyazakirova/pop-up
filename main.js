8e05a4e2a03ff79843eac57a8b150242e60876d5
aliyazakirova <93845260+aliyazakirova@users.noreply.github.com>
2022-06-21 08:51:57 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
+const close = document.getElementsByClassName('close');
 
 open.addEventListener('click', () => {
     modal_container.classList.add('show');
 })
 
-close.addEventListener('click', () => {
-    console.log('opened')
+close[0].addEventListener('click', () => {
     modal_container.classList.remove('show');
 })
+
+/* $("#open").on("click", function(){
+        alert("The paragraph was clicked.");
+        });*/
